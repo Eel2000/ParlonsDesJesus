0d87daa3dce4d084984b36dcc72ecef9a0337149
Eel2000 <eliezerbwana@gmaill.com>
2020-08-28 11:00:44 +0200

    crud for subject

--- Parole/Data/Services/Services.cs
+++ Parole/Data/Services/Services.cs
@@ -1,6 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Parole.Models;
+
 namespace Parole.Data.Services
 {
-    public class Services
+    public class Services : IServices
     {
         private readonly ApplicationDbContext _db;
         public Services(ApplicationDbContext db)
@@ -8,5 +14,91 @@ namespace Parole.Data.Services
             _db = db;
         }
 
+        public async void DeletSubject(int? id)
+        {
+            if(id == null)
+                Debug.WriteLine("id is empty , it does'nt contains anything");
+
+            try
+            {
+                var subjectToDel = await _db.FindAsync<Subject>(id);
+
+                _db.Subjects.Remove(subjectToDel);
+                await _db.SaveChangesAsync();
+            }
+            catch (System.Exception)
+            {
+                Debug.WriteLine("Ooops! Something goes wrong there!");
+                throw;
+            }
+        }
+
+        public Subject DisplaySubject(int? id)
+        {
+            if (id == null)
+            {
+               
+                Debug.WriteLine("Id is null");
+            }
+
+            try
+            {
+                var subjectToDisplay = _db.Find<Subject>(id);
+                return subjectToDisplay;
+            }
+            catch (System.Exception)
+            {
+                Debug.WriteLine("there nothing to display");
+                throw;
+                
+            }
+        }
+
+        public void NewResponse(int idSubject, Response response)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async void NewSubject(Subject subject)
+        {
+            if (!Object.ReferenceEquals(subject,null))
+            {
+                try
+                {
+                    await _db.AddAsync<Subject>(subject);
+                    await _db.SaveChangesAsync();
+                }
+                catch (System.Exception)
+                {
+                    Debug.WriteLine("insert error");
+                    throw;
+                }
+            }
+        }
+
+        public IEnumerable<Response> ResponsesList()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Subject> SubjectList()
+        {
+            try
+            {
+              var subjects = _db.Subjects.ToList();
+
+              if (Object.ReferenceEquals(subjects,null))
+              {
+                  return null;
+              }
+
+              return subjects;
+            }
+            catch (System.Exception)
+            {
+                
+                throw;
+            }
+        }
     }
 }
